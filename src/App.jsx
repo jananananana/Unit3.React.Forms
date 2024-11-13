731146d5c86843d04754ba337ca5589b210ec058
Jana Veliskova <veliskova@gmail.com>
2024-11-12 23:07:42 -0500

    add components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  )
+}
+
+export default App
